94d6c3673c16df13b42f46d659e2e96422b07402
コルク <koluku@users.noreply.github.com>
2016-08-10 06:39:22 +0900

    Update C.py

--- #002/C.py
+++ #002/C.py
@@ -1,9 +1,9 @@
 import math
 
 xa, ya, xb, yb, xc, yc = map(int, input().split())
-a = xb -xa
+a = xb - xa
 b = yb - ya
 c = xc - xa
-d = yc -ya
+d = yc - ya
 area = math.fabs(a * d - b * c) / 2
 print(area)
